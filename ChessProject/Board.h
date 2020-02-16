4da953e0d73f64f0f6d81c79302a231f178f0201
nihilistnun <n.ng@ncl.ac.uk>
2020-02-16 19:54:33 +0000

    updated board header with additional methods and minor rearranging

--- ChessProject/Board.h
+++ ChessProject/Board.h
@@ -9,22 +9,21 @@ using namespace std;
 class Board
 {
 public:
+	//variables
 	static const int BOARD_SIZE = 8;
 	struct Pos {
 		int x, y;
 		Pos(int x, int y); /*{ this->x = x;	this->y = y; } limit to board size*/
-		Pos operator+(const Pos& rhs) const;
-		Pos operator-(const Pos& rhs) const;
-		bool operator==(const Pos& rhs) const; /*{ return x == pos.x && y == pos.y; }*/
+		const Pos operator+(const Pos& rhs) const;
+		const Pos operator-(const Pos& rhs) const;
+		const bool operator==(const Pos& rhs) const; /*{ return x == pos.x && y == pos.y; }*/
+		const string chessPos() const; //convert 0,0 to A1 and 7,7 to h8 etc.
 	};
 	struct Round {
 		string whiteMove, blackMove;
 	};
 	enum class Turn : bool { WHITE, BLACK };
 	/*enum class PieceTypes : int { KING, QUEEN, ROOK, BISHOP, KNIGHT, PAWN, EMPTY };*/
-	//variables
-	vector<Round> history;
-	map<Pos, Piece> whitePieces, blackPieces;
 	//methods
 	const bool move(const Pos pos);
 	void print() const;
@@ -33,7 +32,15 @@ public:
 	const int evaluation() const;
 	const bool check() const;
 	const bool checkMate() const;
+	//get methods
+	void printHistory() const;
+	void printRound(const int index) const;
+
 private:
+	//variables
+	vector<Round> history;
+	map<Pos, Piece> whitePieces, blackPieces;
+	//methods
 	void setup(); // setup the initial chess positions and reset turncount and history
 	void endTurn(); //flip Turn, increment turn count, add to history
 };
